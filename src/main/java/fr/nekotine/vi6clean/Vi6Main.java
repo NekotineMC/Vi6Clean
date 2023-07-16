0741b4a25c8add15658c1cbf70b9c1d0db4c50ba
XxGoldenbluexX <tonyg222@outlook.fr>
2023-07-16 13:48:44 +0200

    oui

--- src/main/java/fr/nekotine/vi6clean/Vi6Main.java
+++ src/main/java/fr/nekotine/vi6clean/Vi6Main.java
@@ -1,25 +1,12 @@
 package fr.nekotine.vi6clean;
 
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import dev.jorel.commandapi.CommandAPI;
-import dev.jorel.commandapi.CommandAPIConfig;
-import fr.nekotine.core.game.GameModeModule;
-import fr.nekotine.core.inventory.menu.MenuModule;
-import fr.nekotine.core.lobby.LobbyModule;
-import fr.nekotine.core.module.ModuleManager;
-import fr.nekotine.core.usable.UsableModule;
-import fr.nekotine.core.visibility.EntityVisibilityModule;
-import fr.nekotine.core.wrapper.WrappingModule;
-import fr.nekotine.vi6clean.impl.game.GM_Vi6;
+import fr.nekotine.core.NekotineCore;
+import fr.nekotine.core.map.MapModule;
 import fr.nekotine.vi6clean.impl.map.MAP_Vi6;
-import fr.nekotine.vi6clean.impl.map.artefact.Artefact;
-import fr.nekotine.vi6clean.impl.map.artefact.BlockArtefactVisual;
-import fr.nekotine.vi6clean.impl.map.artefact.EntityArtefactVisual;
 
-public class Vi6Main extends JavaPlugin implements Listener{
+public class Vi6Main extends JavaPlugin{
 	
 	private static Vi6Main main;
 	
@@ -36,39 +23,22 @@ public class Vi6Main extends JavaPlugin implements Listener{
 	public void onEnable() {
 		main = this;
 		super.onEnable();
-		
-		ModuleManager.Load(this,
-				EntityVisibilityModule.class,
-				WrappingModule.class,
-				UsableModule.class,
-				MenuModule.class
-				);
-		
-		ModuleManager.EnableAll();
-		
-		ModuleManager.GetModule(GameModeModule.class).registerGameMode("vi6", new GM_Vi6(this));
-		
-		ModuleManager.GetModule(LobbyModule.class).registerCommands();
-		
-		registerConfigurationSerializables();
-		
 	}
 	
 	@Override
 	public void onLoad() {
 		super.onLoad();
-		CommandAPI.onLoad(new CommandAPIConfig());
+		NekotineCore.setupFor(this);
+		//CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
+		NekotineCore.MODULES.load(MapModule.class);
+		var commandGenerator = NekotineCore.MODULES.get(MapModule.class).getGenerator();
+		commandGenerator.generateFor(MAP_Vi6.class);
+		commandGenerator.register();
 	}
 	
 	@Override
 	public void onDisable() {
-		ModuleManager.DisableAll();
-	}
-	
-	private void registerConfigurationSerializables() {
-		ConfigurationSerialization.registerClass(MAP_Vi6.class);
-		ConfigurationSerialization.registerClass(Artefact.class);
-		ConfigurationSerialization.registerClass(BlockArtefactVisual.class);
-		ConfigurationSerialization.registerClass(EntityArtefactVisual.class);
+		NekotineCore.MODULES.unloadAll();
+		super.onDisable();
 	}
 }
