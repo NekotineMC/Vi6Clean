093cb2f2b5c0fb732688c84e832777a6ddad73ef
XxGoldenbluexX <tonyg222@outlook.fr>
2023-09-20 14:32:40 +0200

    sonar cooldown visual

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Sonar.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Sonar.java
@@ -43,6 +43,7 @@ public class Sonar extends Tool{
 				(x, y, z) -> {
 					player.spawnParticle(Particle.CRIT_MAGIC, x, y, z, 1, 0, 0, 0, 0, null);
 				});
+		player.setCooldown(getItemStack().getType(), SonarHandler.DELAY_SECOND*20);
 	}
 	
 	@Override
