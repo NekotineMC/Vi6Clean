586251e4d4f6b7f9c414724e24e946ec3bbbb333
XxGoldenbluexX <tonyg222@outlook.fr>
2023-10-18 21:39:29 +0200

    Cloud lantern

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Lantern.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Lantern.java
@@ -1,10 +1,23 @@
 package fr.nekotine.vi6clean.impl.tool.personal;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Transformation;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
 
 import fr.nekotine.core.util.ItemStackUtil;
-import fr.nekotine.vi6clean.constant.Vi6Sound;
+import fr.nekotine.core.util.SpatialUtil;
 import fr.nekotine.vi6clean.constant.Vi6ToolLoreText;
 import fr.nekotine.vi6clean.impl.tool.Tool;
 import net.kyori.adventure.text.Component;
@@ -12,38 +25,93 @@ import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Lantern extends Tool{
 	
-	private boolean canDoubleJump;
+	private ArmorStand fallingArmorStand;
+	
+	private List<BlockDisplay> displayedLanterns = new LinkedList<>();
 	
 	@Override
 	protected ItemStack makeInitialItemStack() {
-		return ItemStackUtil.make(Material.GOLDEN_BOOTS, Component.text("Double Saut", NamedTextColor.GOLD), Vi6ToolLoreText.DOUBLEJUMP.make());
+		return ItemStackUtil.make(Material.LANTERN, LanternHandler.MAX_LANTERN-displayedLanterns.size(), Component.text("Lantern", NamedTextColor.GOLD), Vi6ToolLoreText.LANTERN.make());
 	}
 	
 	@Override
 	protected void cleanup() {
-	}
-
-	public boolean canDoubleJump() {
-		return canDoubleJump;
-	}
-	
-	public void setCanDoubleJump(boolean canDoubleJump) {
-		this.canDoubleJump = canDoubleJump;
-		getOwner().setAllowFlight(canDoubleJump);
+		for(var lantern : displayedLanterns) {
+			lantern.remove();
+		}
+		if (fallingArmorStand != null) {
+			fallingArmorStand.remove();
+		}
 	}
 	
-	public void doubleJump() {
-		var player = getOwner();
-		var loc = player.getLocation();
-		player.setVelocity(player.getVelocity().setY(0.5D));
-		Vi6Sound.DOUBLE_JUMP.play(loc.getWorld(), loc);
-		setCanDoubleJump(false);
+	public boolean tryPlace() {
+		var owner = getOwner();
+		if (displayedLanterns.size() >= LanternHandler.MAX_LANTERN || fallingArmorStand != null || owner == null) {
+			return false;
+		}
+		if (fallingArmorStand != null) {
+			fallingArmorStand.remove();
+		}
+		var loc = owner.getLocation();
+		loc.setYaw(0);
+		loc.setPitch(0);
+		var lantern = (BlockDisplay)owner.getWorld().spawnEntity(loc, EntityType.BLOCK_DISPLAY, SpawnReason.CUSTOM);
+		var transf = new Transformation(
+				new Vector3f(-0.5f, -0.7405f, -0.5f),
+				new AxisAngle4f(),
+				new Vector3f(1, 1, 1),
+				new AxisAngle4f());
+		lantern.setTransformation(transf);
+		lantern.setBlock(Bukkit.createBlockData(Material.LANTERN));
+		fallingArmorStand = (ArmorStand)owner.getWorld().spawnEntity(owner.getLocation(), EntityType.ARMOR_STAND, SpawnReason.CUSTOM);
+		fallingArmorStand.addPassenger(lantern);
+		fallingArmorStand.setInvisible(true);
+		fallingArmorStand.setSilent(true);
+		fallingArmorStand.setInvulnerable(true);
+		fallingArmorStand.setSmall(true);
+		displayedLanterns.add(lantern);
+		getItemStack().setAmount(LanternHandler.MAX_LANTERN-displayedLanterns.size());
+		return true;
 	}
 	
-	public boolean isOnGround() {
-		var player = getOwner();
-		return (!player.isFlying()
-				&& player.getLocation().subtract(0.0D, 0.1D, 0.0D).getBlock().getType().isSolid());
+	public boolean tryPickup(Player picking) {
+		return false;
+	}
+	
+	public void tryRemoveFallingArmorStand() {
+		if (fallingArmorStand != null && fallingArmorStand.isOnGround()) {
+			fallingArmorStand.remove();
+			fallingArmorStand = null;
+		}
+	}
+	
+	public List<BlockDisplay> getDisplayedLanternList(){
+		return displayedLanterns;
+	}
+	
+	public void lanternSmokes() {
+		var owner = getOwner();
+		if (owner == null) {
+			return;
+		}
+		var w = owner.getWorld();
+		var dist = Math.random();
+		for (var lantern : displayedLanterns) {
+			var loc = lantern.getLocation();
+			var x = loc.getX();
+			var y = loc.getY() - 0.74f;
+			var z = loc.getZ();
+			SpatialUtil.circle2DDensity(1.5, 0.5, Math.random() * 6, (offsetX, offsetZ) -> {
+				var scalledOffsetX = offsetX * dist;
+				var scalledOffsetZ = offsetZ * dist;
+				w.spawnParticle(Particle.CAMPFIRE_COSY_SMOKE, x+scalledOffsetX, y, z+scalledOffsetZ, 0, 
+						-offsetX, 0, -offsetZ, 0.01 * dist);
+			});
+			SpatialUtil.circle2DDensity(1.5, 3, Math.random() * 6, (offsetX, offsetZ) -> {
+				w.spawnParticle(Particle.ASH, x+offsetX, y, z+offsetZ, 0, 
+						-offsetX, 0, -offsetZ, 0.01 * dist);
+			});
+		}
 	}
 
 }
--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/LanternHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/LanternHandler.java
@@ -5,33 +5,38 @@ import java.util.List;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
+import fr.nekotine.core.ticking.TickTimeStamp;
+import fr.nekotine.core.ticking.event.TickElapsedEvent;
+import fr.nekotine.core.util.CustomAction;
+import fr.nekotine.core.util.EventUtil;
 import fr.nekotine.vi6clean.constant.Vi6ToolLoreText;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
 import fr.nekotine.vi6clean.impl.tool.ToolType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
-public class LanternHandler extends ToolHandler<DoubleJump>{
+public class LanternHandler extends ToolHandler<Lantern>{
 
-	private static final int MAX_LANTERN = 2;
+	public static final int MAX_LANTERN = 30;
 	
 	public static final List<Component> LORE = Vi6ToolLoreText.LANTERN.make(
 			Placeholder.unparsed("maxlantern", Integer.toString(MAX_LANTERN))
 			);
 	
 	public LanternHandler() {
-		super(ToolType.DOUBLEJUMP, DoubleJump::new);
+		super(ToolType.LANTERN, Lantern::new);
 	}
 	
 	@Override
-	protected void onAttachedToPlayer(DoubleJump tool, Player player) {
+	protected void onAttachedToPlayer(Lantern tool, Player player) {
 	}
 
 	@Override
-	protected void onDetachFromPlayer(DoubleJump tool, Player player) {
+	protected void onDetachFromPlayer(Lantern tool, Player player) {
 		var gm = player.getGameMode();
 		if (gm == GameMode.ADVENTURE || gm == GameMode.SURVIVAL) {
 			player.setAllowFlight(false);
@@ -40,27 +45,31 @@ public class LanternHandler extends ToolHandler<DoubleJump>{
 	
 	@EventHandler
 	private void onPlayerMove(PlayerMoveEvent evt) {
-		for (var tool : getTools()) {
-			if (tool.canDoubleJump()) {
-				continue;
-			}
-			if (evt.getPlayer().equals(tool.getOwner())) {
-				tool.setCanDoubleJump(tool.isOnGround());
-				return;
-			}
+		
+	}
+	
+	@EventHandler
+	private void onPlayerInteract(PlayerInteractEvent evt) {
+		if (evt.getHand() != EquipmentSlot.HAND) {
+			return;
+		}
+		var evtP = evt.getPlayer();
+		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
+		if (optionalTool.isEmpty()) {
+			return;
+		}
+		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY) && optionalTool.get().tryPlace()) {
+			evt.setCancelled(true);
 		}
 	}
 	
 	@EventHandler
-	private void onPlayerToggleFlight(PlayerToggleFlightEvent evt) {
+	private void onTick(TickElapsedEvent evt) {
+		// kill falling armor stand
 		for (var tool : getTools()) {
-			if (!tool.canDoubleJump()) {
-				continue;
-			}
-			if (evt.getPlayer().equals(tool.getOwner())) {
-				tool.doubleJump();
-				evt.setCancelled(true);
-				return;
+			tool.tryRemoveFallingArmorStand();
+			if (evt.timeStampReached(TickTimeStamp.QuartSecond)) {
+				tool.lanternSmokes();
 			}
 		}
 	}
