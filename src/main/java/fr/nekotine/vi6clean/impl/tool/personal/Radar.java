433f85d40b7505e2973c80dceab887cbe1204db4
Axel <axel.vivenot@outlook.fr>
2023-10-27 18:59:14 +0200

    Suite du radar, reste à affiner les particules, son et message

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Radar.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/Radar.java
@@ -2,36 +2,31 @@ package fr.nekotine.vi6clean.impl.tool.personal;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.BlockDisplay;
+import org.bukkit.Particle;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemDisplay;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Transformation;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
-import fr.nekotine.core.inventory.ItemStackBuilder;
+import fr.nekotine.core.NekotineCore;
 import fr.nekotine.core.util.EntityUtil;
 import fr.nekotine.core.util.ItemStackUtil;
+import fr.nekotine.core.wrapper.WrappingModule;
+import fr.nekotine.vi6clean.constant.Vi6Sound;
 import fr.nekotine.vi6clean.impl.tool.Tool;
+import fr.nekotine.vi6clean.impl.wrapper.PlayerWrapper;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Radar extends Tool{
-	private final ItemStack UNPLACED = new ItemStackBuilder(Material.LIGHTNING_ROD)
-			.unstackable()
-			.name(Component.text("Radar", NamedTextColor.GOLD))
-			.lore(RadarHandler.LORE)
-			.build();
-	private final ItemStack PLACED = ItemStackUtil.addEnchant(UNPLACED.clone(), Enchantment.QUICK_CHARGE, 1);
 	private boolean placed = false;
 	private boolean sneaking = false;
-	private BlockDisplay bottom;
-	private BlockDisplay mid;
-	private BlockDisplay top;
+	private ItemDisplay bottom;
+	private ItemDisplay mid;
+	private ItemDisplay top;
+	private int chargeTime;
 	
 	//
-	
+
 	@Override
 	protected ItemStack makeInitialItemStack() {
 		return ItemStackUtil.make(Material.LIGHTNING_ROD, Component.text("Radar", NamedTextColor.GOLD), RadarHandler.LORE);
@@ -54,63 +49,91 @@ public class Radar extends Tool{
 			return false;
 		placed = true;
 			
-		bottom = (BlockDisplay)getOwner().getWorld().spawnEntity(ploc.subtract(0, 0.5, 0), EntityType.BLOCK_DISPLAY);
-		mid = (BlockDisplay)getOwner().getWorld().spawnEntity(ploc.add(0, 1, 0), EntityType.BLOCK_DISPLAY);
-		top = (BlockDisplay)getOwner().getWorld().spawnEntity(ploc.add(0,1.925,0), EntityType.BLOCK_DISPLAY);
+		bottom = (ItemDisplay)getOwner().getWorld().spawnEntity(ploc.subtract(0, 0.5, 0), EntityType.ITEM_DISPLAY);
+		mid = (ItemDisplay)getOwner().getWorld().spawnEntity(ploc.add(0, 1, 0), EntityType.ITEM_DISPLAY);
+		top = (ItemDisplay)getOwner().getWorld().spawnEntity(ploc.add(0,0.8,0), EntityType.ITEM_DISPLAY);
 
 		float y = getOwner().getEyeLocation().getYaw();
 		bottom.setRotation(y + 180, 0);
 		mid.setRotation(y + 180, 0);
 		top.setRotation(y + 180, 0);
 		
-		bottom.setBlock(Material.ORANGE_SHULKER_BOX.createBlockData());;
-		mid.setBlock(Material.LIGHTNING_ROD.createBlockData());
-		top.setBlock(Material.HEAVY_WEIGHTED_PRESSURE_PLATE.createBlockData());
-		Transformation centered = new Transformation(
-				new Vector3f(-0.5f, -0.5f, -0.5f),
-				new Quaternionf(0,0,0,1), 
-				new Vector3f(1,1,1),
-				new Quaternionf(0, 0, 0, 1));
-
-		bottom.setTransformation(centered);
-		mid.setTransformation(centered);
-		top.setTransformation(new Transformation(
-				new Vector3f(-0.5f, -1.1f, -0.5f),
-				new Quaternionf(0.35f, 0, 0, 0.937f), 
-				new Vector3f(1, 2.2f, 1),
-				new Quaternionf(0, 0, 0, 1)));
-		
+		bottom.setItemStack(new ItemStack(Material.ORANGE_SHULKER_BOX));;
+		mid.setItemStack(new ItemStack(Material.LIGHTNING_ROD));
+		top.setItemStack(new ItemStack(Material.DAYLIGHT_DETECTOR));
 		
 		//Faire un son
 		
-		getOwner().setCooldown(getItemStack().getType(), SonarHandler.DELAY_SECOND*20);
-		
 		updateItem();
+		
+		chargeTime = 0;
 		
 		return true;
 	}
 	protected void updateItem() {
 		if(placed) {
-			setItemStack(PLACED);
+			setItemStack(RadarHandler.PLACED);
 		}else {
-			setItemStack(UNPLACED);
+			setItemStack(RadarHandler.UNPLACED);
 		}
 	}
 	protected void detect() {
-		//Animation + detection team ennemi + bruit
 		if(placed) {
+			
+			var opt = NekotineCore.MODULES.get(WrappingModule.class).getWrapperOptional(getOwner(), PlayerWrapper.class);
+			long ennemiNear = opt.get().ennemiTeamInMap().filter(e -> bottom.getLocation().distanceSquared(e.getLocation()) <= RadarHandler.DETECTION_RANGE_SQUARED).count();
+			
+			//Faire un son custom
+			if(ennemiNear > 0)
+				Vi6Sound.SONAR_POSITIVE.play(bottom.getWorld(), bottom.getLocation());
+			
+			//Faire un message avec le nombre de joueurs
+			
+			cleanup();
 			placed = false;
-			bottom.remove();
-			mid.remove();
-			top.remove();
 			updateItem();
 		}
 	}
-	protected void tickParticle() {
-		if(placed || sneaking) {
-			//Fait un cercle de particules
+	protected void tickCharge() {
+		if(placed) {
+			if(chargeTime == 1) {
+				top.setInterpolationDelay(0);
+				top.setInterpolationDuration(RadarHandler.DELAY_SECOND * 20 - 10);
+			    top.setTransformation(RadarHandler.TOP_TRANSFORMATION);
+			}
+			if(++chargeTime>=RadarHandler.DELAY_SECOND * 20) {
+				detect();
+			}
 		}
 		
+	}
+	protected void tickParticle() {
+		if(placed) {
+			Location loc =  bottom.getLocation();
+			var x = loc.getX();
+			var y = loc.getY();
+			var z = loc.getZ();
+			RadarHandler.BALL.forEach(
+				triplet -> {loc.getWorld().spawnParticle(Particle.COMPOSTER, x + triplet.a(), y + triplet.b(), z + triplet.c(), 1, 0, 0, 0, 0, null);
+			});
+			
+			
+		}else if(sneaking) {
+			Location loc = getOwner().getLocation();
+			var x = loc.getX();
+			var y = loc.getY();
+			var z = loc.getZ();
+			RadarHandler.SPHERE.forEach(
+				triplet -> {getOwner().spawnParticle(Particle.COMPOSTER, x + triplet.a(), y + triplet.b(), z + triplet.c(), 1, 0, 0, 0, 0, null);
+			});
+		}
+	}
+	protected void tickSound() {
+		if(placed) {
+			//Faire un son custom
+			Vi6Sound.SONAR_NEGATIVE.play(bottom.getWorld(), bottom.getLocation());
+			
+		}
 	}
 	protected boolean isPlaced() {
 		return placed;
