ba14b058c329d1129842728472aecbad1f700779
XxGoldenbluexX <tonyg222@outlook.fr>
2023-12-05 21:41:46 +0100

    début bush handler

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/bush/BushHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/bush/BushHandler.java
@@ -1,36 +1,20 @@
 package fr.nekotine.vi6clean.impl.tool.personal.bush;
 
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.destroystokyo.paper.MaterialSetTag;
 
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.module.ModuleManager;
-import fr.nekotine.core.status.effect.StatusEffect;
-import fr.nekotine.core.status.effect.StatusEffectModule;
-import fr.nekotine.core.ticking.TickTimeStamp;
 import fr.nekotine.core.ticking.TickingModule;
 import fr.nekotine.core.ticking.event.TickElapsedEvent;
-import fr.nekotine.core.util.CustomAction;
-import fr.nekotine.core.util.EventUtil;
-import fr.nekotine.core.wrapper.WrappingModule;
-import fr.nekotine.vi6clean.constant.Vi6Sound;
-import fr.nekotine.vi6clean.constant.Vi6ToolLoreText;
-import fr.nekotine.vi6clean.impl.status.effect.OmniCaptedStatusEffectType;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
 import fr.nekotine.vi6clean.impl.tool.ToolType;
-import fr.nekotine.vi6clean.impl.wrapper.PlayerWrapper;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 public class BushHandler extends ToolHandler<Bush>{
 
@@ -39,22 +23,17 @@ public class BushHandler extends ToolHandler<Bush>{
 		Ioc.resolve(ModuleManager.class).tryLoad(TickingModule.class);
 	}
 	
-	private static final StatusEffect glowEffect = new StatusEffect(OmniCaptedStatusEffectType.get(), 0);
-	
-	public static final int DETECTION_BLOCK_RANGE = 3;
-	
-	public static final int NB_MAX_WATCHER = 3;
-	
-	public static final int DETECTION_RANGE_SQUARED = DETECTION_BLOCK_RANGE * DETECTION_BLOCK_RANGE;
-	
-	public static final List<Component> LORE = Vi6ToolLoreText.WATCHER.make(
-			Placeholder.unparsed("range", DETECTION_BLOCK_RANGE+" blocs"),
-			Placeholder.parsed("nbmax", Integer.toString(NB_MAX_WATCHER))
+	private final MaterialSetTag bushMaterials = new MaterialSetTag(NamespacedKey.fromString("bush_hideout", Ioc.resolve(JavaPlugin.class)),
+			Material.PEONY, Material.TALL_GRASS, Material.LARGE_FERN,Material.LILAC, Material.ROSE_BUSH, Material.SMALL_DRIPLEAF, Material.KELP
 			);
+	
+	private final double DETECTION_BLOCK_RANGE = Ioc.resolve(Configuration.class).getDouble("tool.bush.reveal_range", 2);
+	
+	private final double DETECTION_RANGE_SQUARED = DETECTION_BLOCK_RANGE * DETECTION_BLOCK_RANGE;
 	
 	@Override
 	protected void onAttachedToPlayer(Bush tool, Player player) {
-		tool.setSneaking(player.isSneaking());
+		tool.setInBush(bushMaterials.isTagged(player.getLocation().getBlock()));
 	}
 
 	@Override
@@ -63,83 +42,14 @@ public class BushHandler extends ToolHandler<Bush>{
 	}
 	
 	@EventHandler
-	private void onPlayerInterract(PlayerInteractEvent evt) {
-		if (evt.getHand() != EquipmentSlot.HAND) {
-			return;
-		}
-		var evtP = evt.getPlayer();
-		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
-		if (optionalTool.isEmpty()) {
-			return;
-		}
-		System.out.println("ACTION = "+evt.getAction());
-		if (EventUtil.isCustomAction(evt, CustomAction.INTERACT_ANY) && optionalTool.get().tryDropWatcher()) {
-			evt.setCancelled(true);
-		}
-		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY) && optionalTool.get().tryPickupWatcher()) {
-			evt.setCancelled(true);
-		}
-	}
-	
-	@EventHandler
-	private void onPlayerToggleSneak(PlayerToggleSneakEvent evt) {
-		var tools = getTools().stream().filter(t -> evt.getPlayer().equals(t.getOwner())).collect(Collectors.toUnmodifiableSet());
-		for (var tool : tools) {
-			tool.setSneaking(evt.isSneaking());
-		}
-	}
-	
-	@EventHandler
 	private void onTick(TickElapsedEvent evt) {
-		var statusEffectModule = Ioc.resolve(StatusEffectModule.class);
 		for (var tool : getTools()) {
-			if (tool.getOwner() == null) {
+			var owner = tool.getOwner();
+			if (owner == null) {
 				continue;
 			}
-			Supplier<Stream<Player>> enemiTeam =
-					Ioc.resolve(WrappingModule.class).getWrapper(tool.getOwner(), PlayerWrapper.class)::ennemiTeamInMap;
-			var watchers = tool.getWatcherList();
-			var toRemove = watchers.stream().filter(sf ->enemiTeam.get().anyMatch(p -> p.getLocation().distanceSquared(sf.getLocation()) <= 1))
-			.collect(Collectors.toCollection(LinkedList::new));
-			for (var sf : toRemove) {
-				sf.remove();
-				watchers.remove(sf);
-				tool.itemUpdate();
-			}
-			
-			Collection<Player> inRange = enemiTeam.get().filter(
-					ennemi -> watchers.stream().anyMatch(
-							sf -> ennemi.getLocation().distanceSquared(sf.getLocation())<= DETECTION_RANGE_SQUARED)
-					)
-			.collect(Collectors.toCollection(LinkedList::new));
-			var oldInRange = tool.getEnnemiesInRange();
-			if (inRange.size() <= 0 && oldInRange.size() <= 0) {
-				continue;
-			}
-			var ite = oldInRange.iterator();
-			while (ite.hasNext()) {
-				var p = ite.next();
-				if (inRange.contains(p)) {
-					inRange.remove(p);
-				}else {
-					statusEffectModule.removeEffect(p, glowEffect);
-					ite.remove();
-				}
-			}
-			for (var p : inRange) {
-				statusEffectModule.addEffect(p, glowEffect);
-				oldInRange.add(p);
-				Vi6Sound.OMNICAPTEUR_DETECT.play(p);
-				var own = tool.getOwner();
-				if (own != null) {
-					Vi6Sound.OMNICAPTEUR_DETECT.play(own);
-				}
-			}
-		}
-		if (evt.timeStampReached(TickTimeStamp.QuartSecond)){
-			for (var tool : getTools()) {
-				tool.lowTick();
-			}
+			tool.setInBush(bushMaterials.isTagged(owner.getLocation().getBlock()));
+			tool.setRevealed(false);// TODO HERE
 		}
 	}
 	
