1111a460fa5db911b127cd696b9c14ce39c6dc6e
XxGoldenbluexX <tonyg222@outlook.fr>
2024-01-06 12:12:50 +0100

    runes

--- src/main/java/fr/nekotine/vi6clean/impl/wrapper/PreparationPhasePlayerWrapper.java
+++ src/main/java/fr/nekotine/vi6clean/impl/wrapper/PreparationPhasePlayerWrapper.java
@@ -5,14 +5,16 @@ import java.util.stream.Collectors;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
+import fr.nekotine.core.inventory.ItemStackBuilder;
 import fr.nekotine.core.inventory.menu.MenuInventory;
+import fr.nekotine.core.inventory.menu.element.ActionMenuItem;
 import fr.nekotine.core.inventory.menu.element.BooleanInputMenuItem;
 import fr.nekotine.core.inventory.menu.element.ComponentDisplayMenuItem;
 import fr.nekotine.core.inventory.menu.layout.ToolbarMenuLayout;
 import fr.nekotine.core.inventory.menu.layout.WrapMenuLayout;
-import fr.nekotine.core.inventory.menu.layout.ToolbarMenuLayout.Direction;
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.util.ItemStackUtil;
 import fr.nekotine.core.wrapper.WrapperBase;
@@ -20,6 +22,7 @@ import fr.nekotine.core.wrapper.WrappingModule;
 import fr.nekotine.vi6clean.impl.game.Vi6Game;
 import fr.nekotine.vi6clean.impl.game.phase.Vi6PhasePreparation;
 import fr.nekotine.vi6clean.impl.map.ThiefSpawn;
+import fr.nekotine.vi6clean.impl.tool.Tool;
 import fr.nekotine.vi6clean.impl.tool.ToolHandlerContainer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -29,6 +32,10 @@ public class PreparationPhasePlayerWrapper extends WrapperBase<Player> {
 	private int money = 1000;
 	
 	private MenuInventory menu;
+	
+	private MenuInventory runesMenu;
+	
+	private Tool rune;
 	
 	private boolean readyForNextPhase;
 	
@@ -41,25 +48,50 @@ public class PreparationPhasePlayerWrapper extends WrapperBase<Player> {
 		if (wrapped==null) {
 			return;
 		}
+		// Menu page
 		moneyIndicator = new ComponentDisplayMenuItem(new ItemStack(Material.GOLD_INGOT), this::getMoneyDisplay);
 		var readyItem = new BooleanInputMenuItem(ItemStackUtil.make(Material.EMERALD_BLOCK, Component.text("Prêt", NamedTextColor.GREEN)),
 				ItemStackUtil.make(Material.REDSTONE_BLOCK, Component.text("En attente", NamedTextColor.RED)),
 				this::isReadyForNextPhase,
 				this::setReadyForNextPhase);
+		var runesItem = new ActionMenuItem(ItemStackUtil.make(Material.TOTEM_OF_UNDYING, Component.text("Runes", NamedTextColor.BLUE)), (e)->{
+			var cli = e.getWhoClicked();
+			if (cli instanceof Player player) {
+				runesMenu.displayTo(player);
+			}
+		});
 		var wrapLayout = new WrapMenuLayout();
 		var team =  Ioc.resolve(WrappingModule.class).getWrapper(wrapped, PlayerWrapper.class).getTeam();
-		for (var tool : Ioc.resolve(ToolHandlerContainer.class).getHandlers().stream()
+		var container = Ioc.resolve(ToolHandlerContainer.class);
+		for (var tool : container.getHandlers().stream()
 				.filter(t -> t.getTeamsAvailableFor().contains(team) && !t.isRune())
 				.collect(Collectors.toCollection(ArrayList::new))){
 			wrapLayout.addElement(tool.getShopMenuItem());
 		}
-		var toolbarRunes = new ToolbarMenuLayout(ItemStackUtil.make(Material.BLUE_STAINED_GLASS_PANE,Component.empty()), wrapLayout, Direction.DOWN);
-		var toolbar = new ToolbarMenuLayout(ItemStackUtil.make(Material.ORANGE_STAINED_GLASS_PANE,Component.empty()), toolbarRunes);
+		var toolbar = new ToolbarMenuLayout(ItemStackUtil.make(Material.ORANGE_STAINED_GLASS_PANE,Component.empty()), wrapLayout);
 		toolbar.addTool(readyItem);
 		toolbar.addTool(moneyIndicator);
-		toolbarRunes.addTool(readyItem);
-		toolbarRunes.addTool(moneyIndicator);
+		toolbar.addTool(runesItem);
 		menu = new MenuInventory(toolbar,6);
+		// Runes page
+		var backItem = new ActionMenuItem(new ItemStackBuilder(Material.PLAYER_HEAD)
+				.skull("76ebaa41d1d405eb6b60845bb9ac724af70e85eac8a96a5544b9e23ad6c96c62")
+				.flags(ItemFlag.values()).name(Component.text("Retour",NamedTextColor.RED)).build(), (e)->{
+			var cli = e.getWhoClicked();
+			if (cli instanceof Player player) {
+				menu.displayTo(player);
+			}
+		});
+		var runesWrapLayout = new WrapMenuLayout();
+		for (var tool : Ioc.resolve(ToolHandlerContainer.class).getHandlers().stream()
+				.filter(t -> t.getTeamsAvailableFor().contains(team) && t.isRune())
+				.collect(Collectors.toCollection(ArrayList::new))){
+			runesWrapLayout.addElement(tool.getShopMenuItem());
+		}
+		var runesToolbar = new ToolbarMenuLayout(ItemStackUtil.make(Material.BLUE_STAINED_GLASS_PANE,Component.empty()), runesWrapLayout);
+		runesToolbar.addTool(readyItem);
+		runesToolbar.addTool(backItem);
+		runesMenu = new MenuInventory(runesToolbar,6);
 	}
 
 	public MenuInventory getMenu() {
@@ -109,5 +141,13 @@ public class PreparationPhasePlayerWrapper extends WrapperBase<Player> {
 	public void setMoney(int money) {
 		this.money = money;
 		moneyIndicator.askRedraw();
+	}
+
+	public Tool getRune() {
+		return rune;
+	}
+
+	public void setRune(Tool rune) {
+		this.rune = rune;
 	}
 }
