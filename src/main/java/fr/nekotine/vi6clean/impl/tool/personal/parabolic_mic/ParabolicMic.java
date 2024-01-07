4a43796f2696b442f57468c67f14a7cc1f226978
Axel <axel.vivenot@outlook.fr>
2024-01-07 19:31:42 +0100

    lore micro parabolique

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/parabolic_mic/ParabolicMic.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/parabolic_mic/ParabolicMic.java
@@ -1,17 +1,12 @@
 package fr.nekotine.vi6clean.impl.tool.personal.parabolic_mic;
 
 import org.bukkit.Material;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.util.ItemStackUtil;
-import fr.nekotine.vi6clean.constant.Vi6ToolLoreText;
 import fr.nekotine.vi6clean.impl.tool.Tool;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 public class ParabolicMic extends Tool{
 	
@@ -19,11 +14,10 @@ public class ParabolicMic extends Tool{
 	
 	@Override
 	protected ItemStack makeInitialItemStack() {
-		return ItemStackUtil.make(Material.CALIBRATED_SCULK_SENSOR,
-				Component.text("Micro parabolique",NamedTextColor.GOLD),
-						Vi6ToolLoreText.INVISNEAK.make(
-								Placeholder.unparsed("range", Ioc.resolve(Configuration.class).getDouble("tool.parabolic_mic.range", 20)+" blocs")
-								));
+		return ItemStackUtil.make(
+				Material.CALIBRATED_SCULK_SENSOR,
+				Ioc.resolve(ParabolicMicHandler.class).getDisplayName(),
+				Ioc.resolve(ParabolicMicHandler.class).getLore());
 	}
 
 	@Override
