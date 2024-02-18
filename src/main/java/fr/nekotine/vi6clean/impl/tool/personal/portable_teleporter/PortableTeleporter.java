884546eed1499bf63be188d1a9460f3e25d98bc4
Axel <axel.vivenot@outlook.fr>
2024-02-18 20:19:25 +0100

    about 3/4 done

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/portable_teleporter/PortableTeleporter.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/portable_teleporter/PortableTeleporter.java
@@ -1,33 +1,144 @@
 package fr.nekotine.vi6clean.impl.tool.personal.portable_teleporter;
 
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.data.type.StructureBlock;
+import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.inventory.ItemStack;
 
+import fr.nekotine.core.ioc.Ioc;
+import fr.nekotine.core.util.EntityUtil;
 import fr.nekotine.vi6clean.impl.tool.Tool;
 
 public class PortableTeleporter extends Tool{
-
+	private class TeleportationPad {
+		protected static final boolean canPlace(Location location) {
+			return location.clone().subtract(0, 0.1, 0).getBlock().isSolid();
+		}
+		private static final float CARPET_SIZE = 0.06f;
+		
+		private BlockDisplay display;
+		private boolean teleporting;
+		private int teleportationDelayTick;
+		private int vfxDelayTick;
+		
+		protected TeleportationPad(Location location) {
+			var padLoc = location.getBlock().getLocation()/*.subtract(0, TEMP, 0)*/;
+			this.display = (BlockDisplay)padLoc.getWorld().spawnEntity(padLoc, EntityType.BLOCK_DISPLAY, SpawnReason.CUSTOM);
+			var transformation = display.getTransformation();
+			transformation.getScale().set(1, CARPET_SIZE, 1);
+			var bd = (StructureBlock)Material.STRUCTURE_BLOCK.createBlockData();
+			bd.setMode(StructureBlock.Mode.CORNER);
+			display.setBlock(bd);
+			display.setTransformation(transformation);
+			
+			//EntityUtil.fixLighting(display);
+		}
+		protected void teleport() {
+			var handler = Ioc.resolve(PortableTeleporterHandler.class);
+			teleporting = true;
+			teleportationDelayTick = handler.getDelayTick();
+			vfxDelayTick = handler.getVfxDelayTick();
+			if(getOwner() != null)
+				EntityUtil.freeze(getOwner());
+		}
+		protected void cleanup() {
+			display.remove();
+		}
+		protected void tickTeleportation() {
+			if(!teleporting) return;
+			if(--teleportationDelayTick == 0) {
+				teleporting = false;
+				if(getOwner() != null) {
+					var disLoc = display.getLocation().add(0.5,0,0.5);
+					getOwner().teleport(disLoc);
+					EntityUtil.unfreeze(getOwner());
+				}
+				//Sound here
+			}
+		}
+		protected void tickVFX() {
+			if(!teleporting) return;
+			if(--vfxDelayTick == 0) {
+				var handler = Ioc.resolve(PortableTeleporterHandler.class);
+				
+				var PLAYER_VFX = handler.getPlayerVFX();
+				var PLAYER_VFX_1 = PLAYER_VFX.a();
+				var PLAYER_VFX_2 = PLAYER_VFX.b();
+				
+				var PAD_VFX = handler.getPadVFX();
+				var PAD_VFX_1 = PAD_VFX.a();
+				var PAD_VFX_2 = PAD_VFX.b();
+				
+				var TELEPORTATION_DELAY_TICK = handler.getDelayTick();
+				var vfxMin = Math.min(PLAYER_VFX_1.size(), PAD_VFX_1.size());				
+				var vfxCount = Math.ceil(((double)(TELEPORTATION_DELAY_TICK - teleportationDelayTick) / TELEPORTATION_DELAY_TICK) * vfxMin);
+				var disLoc = display.getLocation().add(0.5,0,0.5);
+				for(int i=0 ; i < vfxCount ; i++) {
+					
+					var otherEndIndex = vfxMin - i - 1;
+					PAD_VFX_1.get(otherEndIndex).accept(disLoc);
+					PAD_VFX_2.get(otherEndIndex).accept(disLoc);
+					
+					if(getOwner() != null) {
+						var ownLoc = getOwner().getLocation();
+						PLAYER_VFX_1.get(i).accept(ownLoc);
+						PLAYER_VFX_2.get(i).accept(ownLoc);
+					}
+				}	
+				vfxDelayTick = handler.getVfxDelayTick();
+			}
+		}
+		protected boolean isTeleporting() {
+			return teleporting;
+		}
+	}
+	
+	private final ArrayList<TeleportationPad> pads = new ArrayList<TeleportationPad>();
+	
+	//
+	
 	@Override
 	protected ItemStack makeInitialItemStack() {
-		// TODO Auto-generated method stub
-		return null;
+		return Ioc.resolve(PortableTeleporterHandler.class).getItem();
 	}
-
 	@Override
 	protected void cleanup() {
-		// TODO Auto-generated method stub
-		
+		pads.forEach(tp -> tp.cleanup());
 	}
-
 	@Override
 	protected void onEmpStart() {
-		// TODO Auto-generated method stub
-		
+		setItemStack(Ioc.resolve(PortableTeleporterHandler.class).getEmpItem());
 	}
-
 	@Override
 	protected void onEmpEnd() {
-		// TODO Auto-generated method stub
-		
+		setItemStack(Ioc.resolve(PortableTeleporterHandler.class).getItem());
 	}
-
+	
+	//
+	
+	protected boolean tryPlace() {
+		var handler = Ioc.resolve(PortableTeleporterHandler.class);
+		if(pads.size() == handler.getCharges()) return false;
+		
+		var pLoc = getOwner().getLocation();
+		if(!TeleportationPad.canPlace(pLoc)) return false;
+		
+		pads.add(new TeleportationPad(pLoc));
+		return true;
+	}
+	protected boolean tryTeleport(int padIndex) {
+		if(pads.stream().anyMatch(tp -> tp.isTeleporting())) return false;
+		var pad = pads.get(padIndex);
+		pad.teleport();
+		return false;
+	}
+	protected void tick() {
+		pads.forEach(tp -> 
+		{tp.tickTeleportation();tp.tickVFX();});
+	}
 }
--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/portable_teleporter/PortableTeleporterHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/portable_teleporter/PortableTeleporterHandler.java
@@ -1,28 +1,126 @@
 package fr.nekotine.vi6clean.impl.tool.personal.portable_teleporter;
 
-import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+
+import fr.nekotine.core.inventory.ItemStackBuilder;
+import fr.nekotine.core.ticking.event.TickElapsedEvent;
+import fr.nekotine.core.tuple.Pair;
+import fr.nekotine.core.util.CustomAction;
+import fr.nekotine.core.util.EventUtil;
+import fr.nekotine.core.util.SpatialUtil;
 import fr.nekotine.vi6clean.impl.tool.ToolCode;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
+import net.kyori.adventure.text.format.TextDecoration;
 
 @ToolCode("portable_teleporter")
 public class PortableTeleporterHandler extends ToolHandler<PortableTeleporter>{
-
+	private final int CHARGES = getConfiguration().getInt("teleportation_charges",3);
+	private final int DELAY_TICK = (int)(20 * getConfiguration().getDouble("teleportation_delay",4));
+	private final int COOLDOWN_TICK = (int)(20 * getConfiguration().getDouble("teleportation_cooldown",10));
+	private final int VFX_DELAY_TICK = getConfiguration().getInt("vfx_delay",5);
+	private final ItemStack ITEM = new ItemStackBuilder(
+			Material.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE)
+			.lore(getLore())
+			.name(getDisplayName())
+			.flags(ItemFlag.values())
+			.build();
+	private final ItemStack EMP_ITEM = new ItemStackBuilder(
+			Material.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE)
+			.lore(getLore())
+			.name(getDisplayName().decorate(TextDecoration.STRIKETHROUGH))
+			.flags(ItemFlag.values())
+			.build();
+	private final List<Consumer<Location>> PLAYER_VFX_1 = new ArrayList<Consumer<Location>>();
+	private final List<Consumer<Location>> PLAYER_VFX_2 = new ArrayList<Consumer<Location>>();
+	private final List<Consumer<Location>> PAD_VFX_1 = new ArrayList<Consumer<Location>>();
+	private final List<Consumer<Location>> PAD_VFX_2 = new ArrayList<Consumer<Location>>();
+	
 	public PortableTeleporterHandler() {
 		super(PortableTeleporter::new);
-		// TODO Auto-generated constructor stub
+		SpatialUtil.helix(3, 0.75, 3, 0, 0.25, v -> 
+		PLAYER_VFX_1.add(l -> l.getWorld().spawnParticle(Particle.GLOW, l.clone().add(v), 1,0,0,0,0,null)));
+		SpatialUtil.helix(3, 0.75, 3, Math.PI, 0.25, v -> 
+		PLAYER_VFX_2.add(l -> l.getWorld().spawnParticle(Particle.GLOW, l.clone().add(v), 1,0,0,0,0,null)));
+		SpatialUtil.helix(3, 0.75, 3,0, 0.25, v -> 
+		PAD_VFX_1.add(l -> l.getWorld().spawnParticle(Particle.GLOW, l.clone().add(v), 1,0,0,0,0,null)));
+		SpatialUtil.helix(3, 0.75, 3, Math.PI, 0.25, v -> 
+		PAD_VFX_2.add(l -> l.getWorld().spawnParticle(Particle.GLOW, l.clone().add(v), 1,0,0,0,0,null)));
 	}
 
+	//
+	
+	@EventHandler
+	private void onTick(TickElapsedEvent evt) {
+		getTools().forEach(pt -> pt.tick());
+	}
+	
+	@EventHandler
+	private void onPlayerInterract(PlayerInteractEvent evt) {
+		if (evt.getHand() != EquipmentSlot.HAND) {
+			return;
+		}
+		var evtP = evt.getPlayer();
+		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
+		if (optionalTool.isEmpty()) {
+			return;
+		}
+		
+		var tool = optionalTool.get();
+		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY)){
+
+			if(evtP.isSneaking()) {
+				evt.setCancelled(tool.tryPlace());
+			}else {
+				evt.setCancelled(tool.tryTeleport(0));
+			}
+		}
+	}
+	
+	//
+	
 	@Override
 	protected void onAttachedToPlayer(PortableTeleporter tool, Player player) {
-		// TODO Auto-generated method stub
-		
 	}
-
 	@Override
 	protected void onDetachFromPlayer(PortableTeleporter tool, Player player) {
-		// TODO Auto-generated method stub
-		
 	}
 
+	//
+	
+	protected ItemStack getItem() {
+		return ITEM;
+	}
+	protected ItemStack getEmpItem() {
+		return EMP_ITEM;
+	}
+	protected int getCharges() {
+		return CHARGES;
+	}
+	protected int getDelayTick() {
+		return DELAY_TICK;
+	}
+	protected int getCooldownTick() {
+		return COOLDOWN_TICK;
+	}
+	protected Pair<List<Consumer<Location>>,List<Consumer<Location>>> getPlayerVFX(){
+		return Pair.from(PLAYER_VFX_1, PLAYER_VFX_2);
+	}
+	protected Pair<List<Consumer<Location>>,List<Consumer<Location>>> getPadVFX(){
+		return Pair.from(PAD_VFX_1, PAD_VFX_2);
+	}
+	protected int getVfxDelayTick() {
+		return VFX_DELAY_TICK;
+	}
 }
