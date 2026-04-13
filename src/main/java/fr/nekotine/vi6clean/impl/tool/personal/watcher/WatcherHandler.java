8787e1dc3b2b1dcd2a4c0c8faa833f9ee45c1951
XxGoldenbluexX <tonyg222@outlook.fr>
2026-04-13 19:41:02 +0200

    watcher + le tout build

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/watcher/WatcherHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/watcher/WatcherHandler.java
@@ -3,18 +3,25 @@ package fr.nekotine.vi6clean.impl.tool.personal.watcher;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Silverfish;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
+import fr.nekotine.core.inventory.ItemStackBuilder;
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.module.ModuleManager;
 import fr.nekotine.core.status.effect.StatusEffect;
@@ -23,75 +30,94 @@ import fr.nekotine.core.status.flag.StatusFlagModule;
 import fr.nekotine.core.ticking.TickTimeStamp;
 import fr.nekotine.core.ticking.TickingModule;
 import fr.nekotine.core.ticking.event.TickElapsedEvent;
-import fr.nekotine.core.util.CustomAction;
-import fr.nekotine.core.util.EventUtil;
-import fr.nekotine.core.util.ItemStackUtil;
+import fr.nekotine.core.util.InventoryUtil;
+import fr.nekotine.core.util.MobAiUtil;
+import fr.nekotine.core.util.SpatialUtil;
 import fr.nekotine.core.wrapper.WrappingModule;
 import fr.nekotine.vi6clean.constant.Vi6Sound;
 import fr.nekotine.vi6clean.impl.status.effect.OmniCaptedStatusEffectType;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpEndEvent;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpStartEvent;
 import fr.nekotine.vi6clean.impl.status.flag.EmpStatusFlag;
+import fr.nekotine.vi6clean.impl.tool.Tool;
 import fr.nekotine.vi6clean.impl.tool.ToolCode;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
 import fr.nekotine.vi6clean.impl.wrapper.PlayerWrapper;
+import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.util.Tick;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 @ToolCode("watcher")
-public class WatcherHandler extends ToolHandler<Watcher>{
+public class WatcherHandler extends ToolHandler<WatcherHandler.Watcher>{
+	
 	private final StatusEffect permanentGlowEffect = new StatusEffect(OmniCaptedStatusEffectType.get(), 0);
+	
 	private final StatusEffect lastingGlowEffect = new StatusEffect(OmniCaptedStatusEffectType.get(),
 			Tick.tick().fromDuration(Duration.ofSeconds(getConfiguration().getLong("last_delay_second",1))));
+	
+	private final double DETECTION_BLOCK_RANGE = getConfiguration().getDouble("range",5);
+	
+	private final double DETECTION_RANGE_SQUARED = DETECTION_BLOCK_RANGE * DETECTION_BLOCK_RANGE;
+	
+	private final int NB_MAX_WATCHER = getConfiguration().getInt("nbmax",3);
+
 	public WatcherHandler() {
 		super(Watcher::new);
 		Ioc.resolve(ModuleManager.class).tryLoad(TickingModule.class);
 	}
-	private final double DETECTION_BLOCK_RANGE = getConfiguration().getDouble("range",3);
-	private final double DETECTION_RANGE_SQUARED = DETECTION_BLOCK_RANGE * DETECTION_BLOCK_RANGE;
-	private final int NB_MAX_WATCHER = getConfiguration().getInt("nbmax",3);
-	private final ItemStack NOWATCHER_ITEMSTACK = ItemStackUtil.make(
-			Material.ENDERMITE_SPAWN_EGG, 
-			1,
-			getDisplayName(), 
-			getLore());
 	
-	public StatusEffect getPermanentGlowEffect() {
-		return permanentGlowEffect;
-	}
-	
-	@Override
-	protected void onAttachedToPlayer(Watcher tool, Player player) {
-		tool.setSneaking(player.isSneaking());
-	}
-
-	@Override
-	protected void onDetachFromPlayer(Watcher tool, Player player) {
-		tool.cleanup();
-	}
-
 	@EventHandler
 	private void onPlayerInterract(PlayerInteractEvent evt) {
 		if (evt.getHand() != EquipmentSlot.HAND) {
 			return;
 		}
-		var evtP = evt.getPlayer();
-		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
-		if (optionalTool.isEmpty()) {
+		var statusFlagModule = Ioc.resolve(StatusFlagModule.class);
+		var player = evt.getPlayer();
+		var tool = getToolFromItem(evt.getItem());
+		if (tool == null || tool.watchers.size() < NB_MAX_WATCHER || statusFlagModule.hasAny(player, EmpStatusFlag.get())) {
 			return;
 		}
-	
-		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY) && optionalTool.get().tryDropWatcher()) {
+		
+		// TRY PICKUP
+		
+		var ownerLoc = player.getLocation();
+		for (var sf : tool.watchers.stream().filter(sf -> sf.getLocation().distanceSquared(ownerLoc) <= DETECTION_RANGE_SQUARED)
+				.collect(Collectors.toCollection(LinkedList::new))) {
+			tool.watchers.remove(sf);
+			sf.remove();
+			editItem(tool, item -> {
+				item.unsetData(DataComponentTypes.ITEM_MODEL);
+				item.setAmount(NB_MAX_WATCHER-tool.watchers.size());
+			});
 			evt.setCancelled(true);
+			return;
 		}
-		if (EventUtil.isCustomAction(evt, CustomAction.INTERACT_ANY) && optionalTool.get().tryPickupWatcher()) {
-			evt.setCancelled(true);
+		
+		// TRY DROP
+		
+		if (tool.watchers.size() >= NB_MAX_WATCHER) {
+			return;
 		}
-	}
-	
-	@EventHandler
-	private void onPlayerToggleSneak(PlayerToggleSneakEvent evt) {
-		var tools = getTools().stream().filter(t -> evt.getPlayer().equals(t.getOwner())).collect(Collectors.toUnmodifiableSet());
-		for (var tool : tools) {
-			tool.setSneaking(evt.isSneaking());
-		}
+		tool.watchers.add(ownerLoc.getWorld().spawnEntity(ownerLoc, EntityType.SILVERFISH, SpawnReason.CUSTOM, e -> {
+			if (e instanceof Silverfish fish) {
+				e.setInvulnerable(true);
+				e.setSilent(true);
+				e.setPersistent(false);
+				MobAiUtil.clearBrain(fish); // Gravity is disabled if Mob AI is disabled
+			}
+		}));
+		editItem(tool, item -> {
+			var remaining = NB_MAX_WATCHER-tool.watchers.size();
+			if (remaining > 0) {
+				item.setAmount(remaining);
+			}else {
+				item.setData(DataComponentTypes.ITEM_MODEL, Material.ENDERMITE_SPAWN_EGG.key());
+				item.setAmount(1);
+			}
+		});
+		evt.setCancelled(true);
 	}
 	
 	@EventHandler
@@ -99,26 +125,51 @@ public class WatcherHandler extends ToolHandler<Watcher>{
 		var statusEffectModule = Ioc.resolve(StatusEffectModule.class);
 		var statusFlagModule = Ioc.resolve(StatusFlagModule.class);
 		for (var tool : getTools()) {
-			if (tool.getOwner() == null) {
+			var owner = tool.getOwner();
+			if (owner == null) {
 				continue;
 			}
+			
+			// PARTICLE
+			
+			if (evt.timeStampReached(TickTimeStamp.QuartSecond)){
+				if (tool.watchers.size() < NB_MAX_WATCHER) {
+					if (!owner.isSneaking() || !itemMatch(tool, owner.getInventory().getItemInMainHand())) {
+						return;
+					}
+					var loc = owner.getLocation();
+					var x = loc.getX();
+					var y = loc.getY();
+					var z = loc.getZ();
+					SpatialUtil.circle2DDensity(DETECTION_BLOCK_RANGE, 5, 0,
+							(offsetX, offsetZ) -> {
+								owner.spawnParticle(Particle.FIREWORK, x + offsetX, y, z + offsetZ, 1, 0, 0, 0, 0, null);
+							});
+				}
+			}
+			
+			// GLOW EFFECT
+			
 			Supplier<Stream<Player>> enemiTeam =
-					Ioc.resolve(WrappingModule.class).getWrapper(tool.getOwner(), PlayerWrapper.class)::ennemiTeamInMap;
-			var watchers = tool.getWatcherList();
-			var toRemove = watchers.stream().filter(sf ->enemiTeam.get().anyMatch(p -> p.getLocation().distanceSquared(sf.getLocation()) <= 1))
+					Ioc.resolve(WrappingModule.class).getWrapper(owner, PlayerWrapper.class)::ennemiTeamInMap;
+
+			var toRemove = tool.watchers.stream().filter(sf ->enemiTeam.get().anyMatch(p -> p.getLocation().distanceSquared(sf.getLocation()) <= 1))
 			.collect(Collectors.toCollection(LinkedList::new));
 			for (var sf : toRemove) {
 				sf.remove();
-				watchers.remove(sf);
-				tool.itemUpdate();
+				tool.watchers.remove(sf);
+				editItem(tool, item -> {
+					item.unsetData(DataComponentTypes.ITEM_MODEL);
+					item.setAmount(NB_MAX_WATCHER-tool.watchers.size());
+				});
 			}
 			
 			Collection<Player> inRange = enemiTeam.get().filter(
-					ennemi -> watchers.stream().anyMatch(
+					ennemi -> tool.watchers.stream().anyMatch(
 							sf -> ennemi.getLocation().distanceSquared(sf.getLocation())<= DETECTION_RANGE_SQUARED)
 					)
 			.collect(Collectors.toCollection(LinkedList::new));
-			var oldInRange = tool.getEnnemiesInRange();
+			var oldInRange = tool.ennemiesInRange;
 			if (inRange.size() <= 0 && oldInRange.size() <= 0) {
 				continue;
 			}
@@ -134,34 +185,95 @@ public class WatcherHandler extends ToolHandler<Watcher>{
 				}
 			}
 			for (var p : inRange) {
-				var own = tool.getOwner();
-				if(own == null || !statusFlagModule.hasAny(own, EmpStatusFlag.get())) {
+				if(owner == null || !statusFlagModule.hasAny(owner, EmpStatusFlag.get())) {
 					statusEffectModule.addEffect(p, permanentGlowEffect);
 					Vi6Sound.OMNICAPTEUR_DETECT.play(p);
-					if (own != null) {
-						Vi6Sound.OMNICAPTEUR_DETECT.play(own);
+					if (owner != null) {
+						Vi6Sound.OMNICAPTEUR_DETECT.play(owner);
 					}
 				}
 				oldInRange.add(p);
 			}
 		}
-		if (evt.timeStampReached(TickTimeStamp.QuartSecond)){
-			for (var tool : getTools()) {
-				tool.lowTick();
-			}
+	}
+
+	@Override
+	protected void onAttachedToPlayer(Watcher tool) {
+	}
+
+	@Override
+	protected void onDetachFromPlayer(Watcher tool) {
+		onToolCleanup(tool);
+	}
+
+	@Override
+	protected void onToolCleanup(Watcher tool) {
+		for (var watcher : tool.watchers) {
+			watcher.remove();
+		}
+		tool.watchers.clear();
+	}
+
+	@Override
+	protected ItemStack makeItem(Watcher tool) {
+		return new ItemStackBuilder(
+				Material.SILVERFISH_SPAWN_EGG)
+				.amount(NB_MAX_WATCHER)
+				.name(getDisplayName())
+				.lore(getLore())
+				.unstackable()
+				.flags(ItemFlag.values())
+				.build();
+	}
+	
+	@EventHandler
+	private void onEmpStart(EntityEmpStartEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				var tool = getToolFromItem(item);
+				var statusModule = Ioc.resolve(StatusEffectModule.class);
+				for(var victim : tool.ennemiesInRange) {
+					statusModule.removeEffect(victim, permanentGlowEffect);
+				}
+				item.editMeta(m -> m.displayName(getDisplayName().decorate(TextDecoration.STRIKETHROUGH).append(Component.text(" - ")).append(Component.text("Brouillé" , NamedTextColor.RED))));
+			});
 		}
 	}
 	
-	public int getNbMaxWatcher() {
-		return NB_MAX_WATCHER;
+	@EventHandler
+	private void onEmpStop(EntityEmpEndEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				var tool = getToolFromItem(item);
+				var statusModule = Ioc.resolve(StatusEffectModule.class);
+				for(var victim : tool.ennemiesInRange) {
+					statusModule.addEffect(victim, permanentGlowEffect);
+					Vi6Sound.OMNICAPTEUR_DETECT.play(victim);
+				}
+				if(tool.ennemiesInRange.size() > 0) {
+					Vi6Sound.OMNICAPTEUR_DETECT.play(p);
+				}
+				item.editMeta(m -> m.displayName(getDisplayName()));
+				
+				if (NB_MAX_WATCHER - tool.watchers.size() > 0) {
+					item.unsetData(DataComponentTypes.ITEM_MODEL); // back to default model
+				}else {
+					item.setData(DataComponentTypes.ITEM_MODEL, Material.ENDERMITE_SPAWN_EGG.key());
+				}
+			});
+		}
 	}
-	public double getDetectionBlockRange() {
-		return DETECTION_BLOCK_RANGE;
+	
+	public static class Watcher extends Tool{
+		
+		private List<Entity> watchers = new LinkedList<>();
+		
+		private Collection<Player> ennemiesInRange = new LinkedList<>();
+		
+		public Watcher(ToolHandler<?> handler) {
+			super(handler);
+		}
+		
 	}
-	public double getDetectionBlockRangeSquared() {
-		return DETECTION_RANGE_SQUARED;
-	}
-	public ItemStack getNoWatcherItemStack() {
-		return NOWATCHER_ITEMSTACK;
-	}
+	
 }
