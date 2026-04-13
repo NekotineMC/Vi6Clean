b232c8196656e6542cdce34a998263d119bb82e6
XxGoldenbluexX <tonyg222@outlook.fr>
2026-04-13 17:13:48 +0200

    Tazer + Tracker rework

--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/tazer/TazerHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/tazer/TazerHandler.java
@@ -1,81 +1,150 @@
 package fr.nekotine.vi6clean.impl.tool.personal.tazer;
 
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.nekotine.core.inventory.ItemStackBuilder;
-import fr.nekotine.core.ticking.event.TickElapsedEvent;
+import fr.nekotine.core.ioc.Ioc;
+import fr.nekotine.core.status.effect.StatusEffect;
+import fr.nekotine.core.status.effect.StatusEffectModule;
+import fr.nekotine.core.status.flag.StatusFlagModule;
 import fr.nekotine.core.util.CustomAction;
+import fr.nekotine.core.util.EntityUtil;
 import fr.nekotine.core.util.EventUtil;
-import fr.nekotine.core.util.ItemStackUtil;
+import fr.nekotine.core.util.InventoryUtil;
+import fr.nekotine.core.util.SpatialUtil;
+import fr.nekotine.core.wrapper.WrappingModule;
+import fr.nekotine.vi6clean.impl.status.effect.TazedStatusEffectType;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpEndEvent;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpStartEvent;
+import fr.nekotine.vi6clean.impl.status.flag.EmpStatusFlag;
+import fr.nekotine.vi6clean.impl.tool.Tool;
 import fr.nekotine.vi6clean.impl.tool.ToolCode;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
+import fr.nekotine.vi6clean.impl.wrapper.PlayerWrapper;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.SwingAnimation;
+import io.papermc.paper.datacomponent.item.SwingAnimation.Animation;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 @ToolCode("tazer")
-public class TazerHandler extends ToolHandler<Tazer>{
-	private final ItemStack EMP_ITEM = new ItemStackBuilder(
-			Material.IRON_INGOT)
-			.name(getDisplayName().append(Component.text(" - ")).append(Component.text("Brouillé" , NamedTextColor.RED)))
-			.lore(getLore())
-			.build();
-	private final ItemStack ITEM = ItemStackUtil.make(
-			Material.SHEARS, 
-			getDisplayName(), 
-			getLore());
+public class TazerHandler extends ToolHandler<TazerHandler.Tazer>{
+
 	private final int COOLDOWN_TICK = (int)(20*getConfiguration().getDouble("cooldown", 10));
+	
 	private final int TAZED_DURATION_TICK = (int)(20*getConfiguration().getDouble("tazed_duration", 1));
+	
+	private final StatusEffect tazedEffect = new StatusEffect(TazedStatusEffectType.get(), TAZED_DURATION_TICK);
 	
 	public TazerHandler() {
 		super(Tazer::new);
 	}
 	
-	@Override
-	protected void onAttachedToPlayer(Tazer tool, Player player) {
+	@EventHandler
+	private void onPlayerInterract(PlayerInteractEvent evt) {
+		if (evt.getHand() != EquipmentSlot.HAND || !EventUtil.isCustomAction(evt, CustomAction.HIT_ANY)) {
+			return;
+		}
+		var player = evt.getPlayer();
+		var statusFlagModule = Ioc.resolve(StatusFlagModule.class);
+		var tool = getToolFromItem(evt.getItem());
+		if (tool == null || player.getCooldown(NamespacedKey.fromString(getToolCode()+'/'+tool.getId(), Ioc.resolve(JavaPlugin.class))) <= 0 || statusFlagModule.hasAny(player, EmpStatusFlag.get())) {
+			return;
+		}
+		// SHOT
+		
+		var optWrap = Ioc.resolve(WrappingModule.class).getWrapperOptional(player, PlayerWrapper.class);
+		if (optWrap.isEmpty()) {
+			return;
+		}
+		var eyeLoc = player.getEyeLocation();
+		var eyeDir = eyeLoc.getDirection();
+		var world = player.getWorld();
+		var range = 100d;
+		var trace = world.rayTrace(eyeLoc, eyeDir, range, FluidCollisionMode.NEVER, true,1.0, e -> !e.equals(player) && e instanceof LivingEntity);
+		if (trace == null) {
+			return; // No hit, le joueur à tiré en l'air
+		}
+		var hite = trace.getHitEntity();
+		if (hite != null && hite instanceof LivingEntity hit) {
+			EntityUtil.fakeDamage(hit);
+			if (optWrap.get().ennemiTeamInMap().anyMatch(e -> e.equals(hit))) {
+				Ioc.resolve(StatusEffectModule.class).addEffect(hit, tazedEffect);
+			}
+		}
+		var hitp = trace.getHitPosition();
+		if (hitp != null) {
+			range = hitp.distance(eyeLoc.toVector());
+		}
+		SpatialUtil.line3DFromDir(eyeLoc.toVector(), eyeLoc.getDirection(), range, 4,
+				(vec) -> world.spawnParticle(Particle.FIREWORK, vec.getX(), vec.getY(), vec.getZ(), 0,
+						0, 0, 0, 0f));
+		player.setCooldown(NamespacedKey.fromString(getToolCode()+'/'+tool.getId(), Ioc.resolve(JavaPlugin.class)), COOLDOWN_TICK);
+		
+		evt.setCancelled(true);
 	}
 
 	@Override
-	protected void onDetachFromPlayer(Tazer tool, Player player) {
+	protected void onAttachedToPlayer(Tazer tool) {
+	}
+
+	@Override
+	protected void onDetachFromPlayer(Tazer tool) {
+	}
+
+	@Override
+	protected void onToolCleanup(Tazer tool) {
+	}
+
+	@Override
+	protected ItemStack makeItem(Tazer tool) {
+		return new ItemStackBuilder(
+				Material.SHEARS)
+				.name(getDisplayName())
+				.lore(getLore())
+				.flags(ItemFlag.values())
+				.postApply(item -> item.setData(DataComponentTypes.SWING_ANIMATION, SwingAnimation.swingAnimation().type(Animation.NONE)))
+				.build();
 	}
 	
 	@EventHandler
-	private void onTick(TickElapsedEvent evt) {
-		for (var tool : getTools()) {
-			tool.updateCooldown();
+	private void onEmpStart(EntityEmpStartEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				item.setData(DataComponentTypes.ITEM_MODEL, Material.IRON_INGOT.key());
+				item.editMeta(m -> m.displayName(getDisplayName().decorate(TextDecoration.STRIKETHROUGH).append(Component.text(" - ")).append(Component.text("Brouillé" , NamedTextColor.RED))));
+			});
 		}
 	}
 	
 	@EventHandler
-	private void onPlayerInterract(PlayerInteractEvent evt) {
-		if (evt.getHand() != EquipmentSlot.HAND) {
-			return;
-		}
-		var evtP = evt.getPlayer();
-		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
-		if (optionalTool.isEmpty()) {
-			return;
-		}
-		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY) && optionalTool.get().shot()) {
-			evt.setCancelled(true);
+	private void onEmpStop(EntityEmpEndEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				item.unsetData(DataComponentTypes.ITEM_MODEL); // back to default model
+				item.editMeta(m -> m.displayName(getDisplayName()));
+			});
 		}
 	}
 	
-	public ItemStack getItem() {
-		return ITEM;
-	}
-	public ItemStack getEmpItem() {
-		return EMP_ITEM;
-	}
-	public int getCooldownTick() {
-		return COOLDOWN_TICK;
-	}
-	public int getTazedDurationTick() {
-		return TAZED_DURATION_TICK;
+	public static class Tazer extends Tool{
+
+		public Tazer(ToolHandler<?> handler) {
+			super(handler);
+		}
+		
 	}
 	
 }
--- src/main/java/fr/nekotine/vi6clean/impl/tool/personal/tracker/TrackerHandler.java
+++ src/main/java/fr/nekotine/vi6clean/impl/tool/personal/tracker/TrackerHandler.java
@@ -1,18 +1,21 @@
 package fr.nekotine.vi6clean.impl.tool.personal.tracker;
 
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.util.RayTraceResult;
 
 import fr.nekotine.core.inventory.ItemStackBuilder;
 import fr.nekotine.core.ioc.Ioc;
 import fr.nekotine.core.module.ModuleManager;
+import fr.nekotine.core.status.flag.StatusFlagModule;
 import fr.nekotine.core.text.TextModule;
 import fr.nekotine.core.text.style.NekotineStyles;
 import fr.nekotine.core.text.tree.Leaf;
@@ -21,103 +24,176 @@ import fr.nekotine.core.ticking.TickingModule;
 import fr.nekotine.core.ticking.event.TickElapsedEvent;
 import fr.nekotine.core.util.CustomAction;
 import fr.nekotine.core.util.EventUtil;
+import fr.nekotine.core.util.InventoryUtil;
+import fr.nekotine.core.util.SpatialUtil;
+import fr.nekotine.core.wrapper.WrappingModule;
+import fr.nekotine.vi6clean.constant.Vi6Sound;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpEndEvent;
+import fr.nekotine.vi6clean.impl.status.event.EntityEmpStartEvent;
+import fr.nekotine.vi6clean.impl.status.flag.EmpStatusFlag;
+import fr.nekotine.vi6clean.impl.tool.Tool;
 import fr.nekotine.vi6clean.impl.tool.ToolCode;
 import fr.nekotine.vi6clean.impl.tool.ToolHandler;
+import fr.nekotine.vi6clean.impl.wrapper.PlayerWrapper;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.LodestoneTracker;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 @ToolCode("tracker")
-public class TrackerHandler extends ToolHandler<Tracker>{
+public class TrackerHandler extends ToolHandler<TrackerHandler.Tracker>{
 	//private static final int REFRESH_DELAY_SECOND = 2;
+	
 	protected static final double RAY_DISTANCE = 100;
+	
 	protected static final int RAY_SIZE = 0;
-	private final ItemStack GUN_ITEM() {
-		return new ItemStackBuilder(
-		Material.CROSSBOW)
-		.name(getDisplayName().append(Component.text(" - ").append(Component.text("Armé",NamedTextColor.AQUA))))
-		.lore(getLore())
-		.unstackable()
-		.flags(ItemFlag.values())
-		.build();
-	}
-	private final ItemStack EMP_ITEM() {
-		return new ItemStackBuilder(
-				Material.RECOVERY_COMPASS)
-				.name(getDisplayName().append(Component.text(" - ").append(Component.text("Brouillé",NamedTextColor.RED))))
-				.lore(getLore())
-				.unstackable()
-				.flags(ItemFlag.values())
-				.build();
-	}
 	
 	//private int n = 0;
 	public TrackerHandler() {
 		super(Tracker::new);
 		Ioc.resolve(ModuleManager.class).tryLoad(TickingModule.class);
 	}
-	@Override
-	protected void onAttachedToPlayer(Tracker tool, Player player) {
-	}
-	@Override
-	protected void onDetachFromPlayer(Tracker tool, Player player) {
-	}
-	
 	//
 	
 	@EventHandler
 	private void onTick(TickElapsedEvent evt) {
 		if(evt.timeStampReached(TickTimeStamp.Second)/* && ++n>=REFRESH_DELAY_SECOND*/) {
 			//n = 0;
+			var textModule = Ioc.resolve(TextModule.class);
+			var statusFlagModule = Ioc.resolve(StatusFlagModule.class);
 			for (var tool : getTools()) {
-				tool.tickRefresh();
+				var owner = tool.getOwner();
+				if (tool.tracked != null && !statusFlagModule.hasAny(owner, EmpStatusFlag.get())) {
+					var distance = owner.getLocation().distance(tool.tracked.getLocation());
+					editItem(tool, item -> {
+						item.editMeta(meta -> {
+							meta.displayName(getDisplayName().append(Component.text(" - ").append(textModule.message(Leaf.builder()
+									.addLine("<red>Distance: <aqua><distance>m")
+									.addStyle(Placeholder.unparsed("distance", String.valueOf((int)distance)))
+									.addStyle(NekotineStyles.STANDART)).buildFirst())));
+						});
+						item.setData(DataComponentTypes.LODESTONE_TRACKER, LodestoneTracker.lodestoneTracker(tool.tracked.getLocation(), false));
+					});
+				}
 			}
 		}
 	}
 	@EventHandler
 	private void onPlayerInterract(PlayerInteractEvent evt) {
-		if (evt.getHand() != EquipmentSlot.HAND) {
-			return;
-		}
-		var evtP = evt.getPlayer();
-		var optionalTool = getTools().stream().filter(t -> evtP.equals(t.getOwner()) && t.getItemStack().isSimilar(evt.getItem())).findFirst();
-		if (optionalTool.isEmpty()) {
+		if (evt.getHand() != EquipmentSlot.HAND && !EventUtil.isCustomAction(evt, CustomAction.HIT_ANY)) {
 			return;
 		}
 		
-		if(EventUtil.isCustomAction(evt, CustomAction.INTERACT_ANY)) {
-			evt.setCancelled(true);
+		var statusFlagModule = Ioc.resolve(StatusFlagModule.class);
+		var player = evt.getPlayer();
+		var tool = getToolFromItem(evt.getItem());
+		if (tool == null || tool.tracked != null || statusFlagModule.hasAny(player, EmpStatusFlag.get())) {
+			return;
 		}
-		if (EventUtil.isCustomAction(evt, CustomAction.HIT_ANY) && optionalTool.get().shoot(this)) {
-			evt.setCancelled(true);
+		
+		// SHOOT
+		var ownWrap = Ioc.resolve(WrappingModule.class).getWrapper(player, PlayerWrapper.class);
+		var eyeLoc = player.getEyeLocation();
+		RayTraceResult res = player.getWorld().rayTrace(
+				eyeLoc, 
+				eyeLoc.getDirection(), 
+				TrackerHandler.RAY_DISTANCE, 
+				FluidCollisionMode.NEVER, 
+				true, 
+				TrackerHandler.RAY_SIZE, 
+				hit -> ownWrap.ennemiTeamInMap().anyMatch(ennemi -> ennemi.equals(hit)));
+		
+		var range = TrackerHandler.RAY_DISTANCE;
+		if(res!=null) {
+			var hitP = res.getHitPosition();
+			range = eyeLoc.distance(new Location(eyeLoc.getWorld(), hitP.getX(), hitP.getY(), hitP.getZ()));
+		}
+		SpatialUtil.line3DFromDir(
+			eyeLoc.getX(),
+			eyeLoc.getY(),
+			eyeLoc.getZ(),
+			eyeLoc.getDirection(),
+			range, 
+			4, 
+			(vec) -> player.spawnParticle(Particle.FIREWORK, vec.getX(), vec.getY(), vec.getZ(), 0, 0, 0, 0, 0f));
+		
+		if(res == null || res.getHitEntity()==null) {
+			remove(tool);
+			Vi6Sound.TRACKER_FAIL.play(player);
+			return;
+		}
+		
+		if (res.getHitEntity() instanceof Player hit) { // Always the case
+			tool.tracked = hit;
+		}
+		Vi6Sound.TRACKER_SUCCESS.play(player);
+		//kb & animation
+		tool.tracked.playHurtAnimation(player.getEyeLocation().getYaw() + tool.tracked.getLocation().getYaw());
+		editItem(tool, item -> {
+			item.setData(DataComponentTypes.ITEM_MODEL, Material.COMPASS.key());
+		});
+		evt.setCancelled(true);
+	}
+	
+	@Override
+	protected void onAttachedToPlayer(Tracker tool) {
+	}
+	
+	@Override
+	protected void onDetachFromPlayer(Tracker tool) {
+	}
+	
+	@Override
+	protected void onToolCleanup(Tracker tool) {
+	}
+	
+	@Override
+	protected ItemStack makeItem(Tracker tool) {
+		return new ItemStackBuilder(
+				Material.CROSSBOW)
+				.name(getDisplayName().append(Component.text(" - ").append(Component.text("Armé",NamedTextColor.AQUA))))
+				.lore(getLore())
+				.unstackable()
+				.flags(ItemFlag.values())
+				.build();
+	}
+	
+	@EventHandler
+	private void onEmpStart(EntityEmpStartEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				item.setData(DataComponentTypes.ITEM_MODEL, Material.RECOVERY_COMPASS.key());
+				item.editMeta(m -> m.displayName(getDisplayName().decorate(TextDecoration.STRIKETHROUGH).append(Component.text(" - ")).append(Component.text("Brouillé" , NamedTextColor.RED))));
+				item.unsetData(DataComponentTypes.LODESTONE_TRACKER);
+			});
 		}
 	}
 	
-	public ItemStack getGunItem() {
-		return GUN_ITEM();
+	@EventHandler
+	private void onEmpStop(EntityEmpEndEvent evt) {
+		if (evt.getEntity() instanceof Player p) {
+			InventoryUtil.taggedItems(p.getInventory(), TOOL_TYPE_KEY, getToolCode()).forEach(item -> {
+				var tool = getToolFromItem(item);
+				if (tool.tracked == null) {
+					item.unsetData(DataComponentTypes.ITEM_MODEL); // back to default model
+					item.editMeta(m -> m.displayName(getDisplayName().append(Component.text(" - ").append(Component.text("Armé",NamedTextColor.AQUA)))));
+				}else {
+					item.setData(DataComponentTypes.ITEM_MODEL, Material.COMPASS.key());
+				}
+			});
+		}
 	}
-	public ItemStack getEmpItem() {
-		return EMP_ITEM();
-	}
-	public final ItemStack getCompassItem(Player owner, Location hitLoc) {
-		var distance = owner.getLocation().distance(hitLoc);
-		Component name = Ioc.resolve(TextModule.class).message(Leaf.builder()
-				.addLine("<gold>Traceur</gold> - <red>Distance: <aqua><distance>m")
-				.addStyle(Placeholder.unparsed("distance", String.valueOf((int)distance)))
-				.addStyle(NekotineStyles.STANDART)).buildFirst();	
-				
-		var item = new ItemStackBuilder(Material.COMPASS)
-		.name(name)
-		.lore(getLore())
-		.unstackable()
-		.flags(ItemFlag.values())
-		.build();
+	
+	public static class Tracker extends Tool{
 
-		var meta = (CompassMeta)item.getItemMeta();
-		meta.setLodestoneTracked(false);
-		meta.setLodestone(hitLoc);
-		item.setItemMeta(meta);
-
-		return item;
+		private Player tracked;
+		
+		public Tracker(ToolHandler<?> handler) {
+			super(handler);
+		}
+		
 	}
+	
 }
