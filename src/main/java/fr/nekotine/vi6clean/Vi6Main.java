54670a76fb77c8e7398984861c2d95ebbe18d41d
XxGoldenbluexX <tonyg222@outlook.fr>
2026-06-17 15:42:09 +0200

    Ajout du resourcepack server via le plugin

--- src/main/java/fr/nekotine/vi6clean/Vi6Main.java
+++ src/main/java/fr/nekotine/vi6clean/Vi6Main.java
@@ -1,8 +1,11 @@
 package fr.nekotine.vi6clean;
 
+import java.net.URI;
 import java.nio.file.FileSystems;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchService;
+import java.util.Scanner;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -32,7 +35,11 @@ import fr.nekotine.vi6clean.impl.tool.ToolHandlerContainer;
 import fr.nekotine.vi6clean.impl.wrapper.LobbyPhasePlayerWrapper;
 import fr.nekotine.vi6clean.voicechat.Vi6VoiceChatPlugin;
 import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.event.connection.configuration.PlayerConnectionInitialConfigureEvent;
+import net.kyori.adventure.resource.ResourcePackInfo;
+import net.kyori.adventure.resource.ResourcePackRequest;
 import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 
 public class Vi6Main extends NekotinePlugin implements Listener {
@@ -41,11 +48,14 @@ public class Vi6Main extends NekotinePlugin implements Listener {
 
 	private WatchService ws;
 
+	private ResourcePackInfo vi6CleanResourcePack;
+
 	@Override
 	public void onLoad() {
 		super.onLoad();
 		mapCommandsFor(Vi6Map.class);
 		gameCommands();
+		makeResourcePackInfo();
 	}
 
 	@Override
@@ -152,5 +162,34 @@ public class Vi6Main extends NekotinePlugin implements Listener {
 		if (equipable != null && equipable.slot() == EquipmentSlot.HAND) {
 			player.playSound(Sound.sound(equipable.equipSound(), Sound.Source.MASTER, 1, 1));
 		}
+	}
+
+	private void makeResourcePackInfo() {
+		var uuid = UUID.fromString("34ec4879-e002-413b-9fab-4a4dc7949b11");
+		var baseurl = "https://github.com/NekotineMC/Vi6Clean/releases/latest/download/";
+		var packurl = baseurl + "Vi6Clean.zip";
+		var checksumurl = packurl + ".sha1";
+		String sha;
+		logger.info("Lecture du sha1 du resourcepack depuis l'url suivante: " + checksumurl);
+		try (var scanner = new Scanner(URI.create(checksumurl).toURL().openStream())) {
+			scanner.useDelimiter("\\A");
+			sha = scanner.hasNext() ? scanner.next().trim() : "";
+		} catch (Exception e) {
+			logger.error("Une erreur est survenue lors de la lecture du sha1", e);
+			throw new RuntimeException(e); // sad
+		}
+		logger.info("Le sha1 du resourcepack est le suivant: " + sha);
+		vi6CleanResourcePack = ResourcePackInfo.resourcePackInfo(uuid, URI.create(packurl), sha);
+	}
+
+	@EventHandler
+	public void onPlayerInitialConfigure(PlayerConnectionInitialConfigureEvent evt) {
+		var aud = evt.getConnection().getAudience();
+		aud.sendMessage(Component.text("Bonjour " + evt.getConnection().getAddress()));
+		var request = ResourcePackRequest.resourcePackRequest();
+		request.packs(vi6CleanResourcePack);
+		request.required(true)
+				.prompt(Component.text("Le pack de resource du Vi6 est nécessaire pour éviter les erreurs visuelles"));
+		aud.sendResourcePacks(request);
 	}
 }
